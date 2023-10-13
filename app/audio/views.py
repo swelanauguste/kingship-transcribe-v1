9b106e3e9c37b28d901993d556e210a74992b50e
swelanauguste <swelanauguste@gmail.com>
2023-10-13 20:00:25 +0000

    update

--- app/audio/views.py
+++ app/audio/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from .forms import TranscribeAudioForm
@@ -10,10 +11,11 @@ def create_transcription(request):
     if request.method == 'POST':
         form = TranscribeAudioForm(request.POST, request.FILES)
         if form.is_valid():
-            uploaded_file = request.FILES['audio_file']
+            # uploaded_file = request.FILES['audio_file']
             form.save()
             transcribe_audio.delay(form.instance.id)
-            return redirect('/')
+            # return redirect('/')
+            return HttpResponseRedirect('/')
     else:
         form = TranscribeAudioForm()
     return render(request, 'audio/transcribe_audio.html', {'form': form})
